8b0ef3f33d8dadfef8729324947e8667af9b1b84
Chloe Gell <gellert.chloe@gmail.com>
2020-02-07 00:21:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/[A-Z]\w\W/) 
+  if(text.match(/[A-Z]\w\W/))
+    return true 
+  else 
+    return false 
+  end 
 end
 
 def valid_phone_number?(phone)
