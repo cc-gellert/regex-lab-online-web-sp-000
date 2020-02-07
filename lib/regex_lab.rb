6e3980f5765094d108032c4c4a99963f372d987e
Chloe Gell <gellert.chloe@gmail.com>
2020-02-07 00:03:37 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  return word.start_with?(/a|e|i|o|u/) 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/[a-zA-Z]{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(//)
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(//)
 end
